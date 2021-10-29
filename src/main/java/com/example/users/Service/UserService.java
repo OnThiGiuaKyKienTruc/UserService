f53e64687cdbb40d3bfc8659ae7b2692c37c6ab6
ThanhVinh2712 <you@example.com>
2021-10-29 20:53:17 +0700

    RateLimiter

--- src/main/java/com/example/users/Service/UserService.java
+++ src/main/java/com/example/users/Service/UserService.java
@@ -4,6 +4,7 @@ import com.example.users.Entity.User;
 import com.example.users.Repository.UserRepository;
 import com.example.users.VO.Order;
 import com.example.users.VO.ResponseTemplateVO;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,6 +22,7 @@ public class UserService {
     }
 
     @Retry(name = "basic")
+    @RateLimiter(name = "basicExample")
     public ResponseTemplateVO getUserWithDepartment(Long userId) {
         ResponseTemplateVO vo = new ResponseTemplateVO();
         User user = userRepository.findById(userId).get();
